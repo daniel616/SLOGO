db7fd2f327998f9601ed790265def9a66c21a703
Daniel Li <danielli@Daniels-MacBook-Pro.local>
2017-09-02 16:17:44 -0400

    added some fun code libraries to load

--- src/xml/XMLWriter.java
+++ src/xml/XMLWriter.java
@@ -1,4 +1,5 @@
 package xml;
+
 import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,16 +16,18 @@ public abstract class XMLWriter {
 	public static final String WORKSPACE = "Workspace";
 	public static final String LANGUAGE = "Language";
 	public static final DocumentBuilder DOCUMENT_BUILDER = getDocumentBuilder();
-	
+
 	private Element root;
 	private Document doc;
 	private String address;
-	
+
 	public XMLWriter(String pAdd) throws XMLException {
 		doc = DOCUMENT_BUILDER.newDocument();
 		address = pAdd;
 	}
+
 	protected abstract void writeElements() throws XMLException;
+
 	protected void writeRoot(String lan) {
 		root = doc.createElement(WORKSPACE);
 		Attr attr = doc.createAttribute(LANGUAGE);
@@ -32,38 +35,41 @@ public abstract class XMLWriter {
 		root.setAttributeNode(attr);
 		doc.appendChild(root);
 	}
+
 	protected void addElement(Element cRoot, String name, String value) {
 		Element newElement = doc.createElement(name);
 		newElement.appendChild(doc.createTextNode(value));
 		cRoot.appendChild(newElement);
 	}
+
 	protected void addElement(String name, String value) {
 		addElement(root, name, value);
 	}
+
 	protected Element createElement(String el) {
 		return doc.createElement(el);
-	}	
-	
+	}
+
 	protected void appendChild(Element el) {
 		root.appendChild(el);
 	}
-	
+
 	protected void saveFile() throws XMLException {
-		try{
+		try {
 			Transformer transformer = TransformerFactory.newInstance().newTransformer();
 			DOMSource source = new DOMSource(doc);
 			StreamResult result = new StreamResult(new File(address));
 			transformer.transform(source, result);
 		} catch (Exception e) {
 			throw new XMLException("Couldn't create file at: " + address);
-		} 
+		}
 	}
-	private static DocumentBuilder getDocumentBuilder () {
+
+	private static DocumentBuilder getDocumentBuilder() {
 		try {
 			return DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		}
-		catch (ParserConfigurationException e) {
+		} catch (ParserConfigurationException e) {
 			throw new XMLException(e);
-		}	
+		}
 	}
 }
